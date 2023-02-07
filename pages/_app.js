fd329c73975780a16833e3313aa1a05a85e5e99e
arthur <arthursteiner@gmail.com>
2023-02-07 21:48:14 +0900

    Open Zeppelin intergration

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,20 @@ import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Mumbai;
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider 
+  sdkOptions={{
+    gasless: {
+      openzeppelin: {
+        relayerUrl: process.env.NEXT_PUBLIC_OPENZEPPELIN_URL,
+      },
+    },
+  }}
+    desiredChainId={activeChainId}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,20 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { Web3Button } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const tokenId = 0;
+  const quantity = 1;
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Web3Button
+        contractAddress="0xcDFA0Db479d35e1D1C76bf2D65166e32Ce42F048"
+        action={(contract) => contract.erc1155.claim(tokenId, quantity)}
+        onSuccess={() => alert("Claimed!")}
+        onError={() => alert("Something went wrong")}
+      >
+        Claim
+      </Web3Button>
     </div>
   );
 }
